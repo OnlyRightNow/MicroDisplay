de1d0506798c8d340121af49f00f55b66fa2d5f5
Sebastian Beetschen <yodaxx3@gmail.com>
2020-10-29 10:04:47 +0100

    + backup commit

---
+++ AP_ST/AP_ST.ino
@@ -0,0 +1,27 @@
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+WiFiMulti wifiMulti;
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_AP_STA);
+  wifiMulti.addAP("Admiral Boom", "du kommst hier ned rein hahaha");
+  wifiMulti.addAP("Pingu 1", "WG 3.OG li St0773r");
+  wifiMulti.addAP("Pingu 2", "WG 3.OG li St0773r");
+  WiFi.softAP("TEST-ESP", "12345678");
+  if (wifiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("WiFi name: ");
+    Serial.println(WiFi.SSID());
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+2. To be honest, Marc is not often in the group, he is mostly at microsoft. Most of the exchange and coordination is done by a very senior post-doc, Martin Oswald. He is aware of Siyus project and there is quite an active collaboration among the different CV groups at ETH. The physics-property dataset is a project where many people of the lab are very interested in, since it would allow many new things. At the same time some are currently thinking of extending the ETH3D dataset, and this could maybe even be added as well. 
+}
